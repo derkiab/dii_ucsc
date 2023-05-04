2e3151b9962902353df3c5c521931a3bbc13b9b2
derkiab <110829988+derkiab@users.noreply.github.com>
2023-05-03 22:40:44 -0400

    dark,light mode logo fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="w-full fill-current center" style="width: 20rem;"/>
+                        <x-application-logo class="w-full center dark:fill-slate-200 fill-black" style="width: 20rem;"/>
                     </a>
                 </div>
 
@@ -72,6 +72,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('publicaciones')" :active="request()->routeIs('publicaciones')">
+                {{ __('Publicaciones') }}
             </x-responsive-nav-link>
         </div>
 
