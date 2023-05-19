6319c88b19fd9509ac1afc7029ea8be573ad48fd
derkiab <110829988+derkiab@users.noreply.github.com>
2023-05-18 22:48:42 -0400

    stopped using cdn for datatables

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,205 +2,148 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 import 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js';
-import 'https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js';
+import DataTables from 'datatables.net';
+
 import Swal from 'sweetalert2';
-import {createApp} from 'vue/dist/vue.esm-bundler.js';
-import Home from './Home.vue';
 
 
-window.Alpine = Alpine;
 
-Alpine.start();
 
-const app = createApp({});
 
-app.component('home-component', Home);
-app.mount("#app");
 
 $(document).ready(function () {
-    $('#users-table').DataTable({
+    window.Alpine = Alpine;
+
+    Alpine.start();
+
+    new DataTables('#careers-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-$(document).ready(function () {
-    $('#academics-table').DataTable({
+    new DataTables('#users-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-$(document).ready(function () {
-    $('#administratives-table').DataTable({
+    new DataTables('#academics-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#post_students-table').DataTable({
+    new DataTables('#administratives-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#pre_students-table').DataTable({
+    new DataTables('#post_students-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#researchers-table').DataTable({
+    new DataTables('#pre_students-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#part_times-table').DataTable({
+    new DataTables('#researchers-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#publications-table').DataTable({
+    new DataTables('#centers-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#careers-table').DataTable({
+    new DataTables('#part_times-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#dependencies-table').DataTable({
+    new DataTables('#dependencies-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#centers-table').DataTable({
+    new DataTables('#departments-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
-
-
     });
-});
-
-$(document).ready(function () {
-    $('#departments-table').DataTable({
+    new DataTables('#publications-table', {
         dom: 'lfrtip',
 
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
         },
+    });
+    $(document).on('click', '.delete-user', function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
 
+        Swal.fire({
 
+            title: '¿Estás seguro?',
+            text: "¡No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '¡Sí, bórralo!',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    type: 'DELETE',
+                    url: '/users/' + id,
+                    data: {
+                        id: id,
+                        _token: $('meta[name="csrf-token"]').attr('content')
+                    },
+                    success: function (data) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Exito',
+                            text: '¡Usuario eliminado correctamente!',
+                            timer: 1500
+                        });
+                        setTimeout(function () {
+                            location.reload();
+                        }, 1000); // delay for half a second
+                    },
+                    error: function (xhr, status, error) {
+                        console.log(xhr.responseText);
+                    }
+                });
+            }
+        });
     });
 });
 
 
-$(document).on('click', '.delete-user', function (e) {
-    e.preventDefault();
-    var id = $(this).data('id');
-
-    Swal.fire({
-
-        title: '¿Estás seguro?',
-        text: "¡No podrás revertir esto!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: '¡Sí, bórralo!',
-        cancelButtonText: 'Cancelar'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                type: 'DELETE',
-                url: '/users/' + id,
-                data: {
-                    id: id,
-                    _token: $('meta[name="csrf-token"]').attr('content')
-                },
-                success: function (data) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Exito',
-                        text: '¡Usuario eliminado correctamente!',
-                        timer: 1500
-                    });
-                    setTimeout(function () {
-                        location.reload();
-                    }, 1000); // delay for half a second
-                },
-                error: function (xhr, status, error) {
-                    console.log(xhr.responseText);
-                }
-            });
-        }
-    });
-});
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import DataTables from 'datatables.net';
 
 export default defineConfig({
     plugins: [
+
         vue(),
         laravel({
             input: [
@@ -12,6 +14,10 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+
     ],
+    optimizeDeps: {
+        include: ['datatables.net'],
+      },
 
 });
